7d1b99cf99aae75e50cda41cc96c1f0a9e42958c
adamferreira <adam.ferreira.dc@gmail.com>
2023-05-23 18:39:30 +0200

    Dataframe Wrappers

--- pypkm/data/df_types.py
+++ pypkm/data/df_types.py
@@ -2,3 +2,17 @@ import pandas as pd
 
 class DataFrameWrapper(object):
     pass
+
+class Pokedex(DataFrameWrapper):
+    pass
+
+class Moves(DataFrameWrapper):
+    pass
+
+class Movesets(DataFrameWrapper):
+    pass
+class Abilities(DataFrameWrapper):
+    pass
+
+class Natures(DataFrameWrapper):
+    pass
